8e3f051cee5cfb855953e90c5eb4cc7f20b8072a
vishveshshukal109-collab <vishveshshukal109@gmail.com>
2026-04-13 16:03:10 +0530

    Middleware to handle CORS

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,20 @@
 import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
+
+// Middleware to handle CORS
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173', // Allow requests from the frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'] // Allowed headers
+}))
+
+// Middleware to handle JSON object in req body
+app.use(express.json())
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
