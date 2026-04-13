ac596dd928b590dfa8758d90553e2292b7f81330
vishveshshukal109-collab <vishveshshukal109@gmail.com>
2026-04-13 15:23:31 +0530

    Add Pages and routes

---
+++ Frontend/src/routes/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const PrivateRoute = ({ allowedRoles }) => {
+  return (
+    <Outlet/>
+  )
+}
+
+export default PrivateRoute
