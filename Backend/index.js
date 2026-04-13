2ef26482e6b30d8e11071d5446912ade20e3b638
vishveshshukal109-collab <vishveshshukal109@gmail.com>
2026-04-13 16:38:53 +0530

    Connected to database

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Database connected successfully')
+}).catch((error) => {
+    console.error('Database connection error:')
+})
 
 const app = express()
 
