49af229fa3c16e602c28e6ac30ec227ce9e3c6df
vishveshshukal109-collab <vishveshshukal109@gmail.com>
2026-04-14 13:23:36 +0530

    get user profile api route

--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { signin, signup } from '../controller/auth.controller.js';
+import { signin, signup, userProfile } from '../controller/auth.controller.js';
+import { verifytoken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 
 router.post('/signin', signin);
+
+router.get('/userProfile', verifytoken, userProfile);
 
 export default router; 
---
+++ Backend/utils/verifyUser.js
@@ -0,0 +1,30 @@
+import user from "../models/user.model.js";
+import { errorHandler } from "./error.js";
+import jwt from "jsonwebtoken";
+
+export const verifytoken = (req, res, next) => {
+    const token = req.cookies.access_token;
+
+    if (!token) {
+        return next(errorHandler(401, "Unauthorized"));
+
+    }
+jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+        return next(errorHandler(403, "Unauthorized"));
+    }
+
+    req.user = user;
+    next();
+
+})
+}
+
+
+export const adminOnly = (req, res, next) => {
+    if (req.user && req.user.role === "admin"){
+        next();
+    }else{
+        return next(errorHandler(403, "Access denied, Admin only! "));
+}
+}
