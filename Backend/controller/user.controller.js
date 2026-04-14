e68c65e054450eee7a6037cc88f43248cf02fd4c
vishveshshukal109-collab <vishveshshukal109@gmail.com>
2026-04-14 20:41:11 +0530

    get user by id api route

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,5 +1,6 @@
 import Task from "../models/task.model.js";
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const getUsers = async (req, res, next) => {
 try {
@@ -34,4 +35,22 @@ try {
     next(error);
 }
 
+}
+
+
+export const getUserById = async (req, res, next) => {
+    try {
+        
+        const user = await User.findById(req.params.id).select("-password");
+
+        if (!user) {
+            return next(errorHandler(404, "User not found"));
+        }
+        res.status(200).json(user);
+
+
+    } catch (error) {
+        next(error);
+        
+    }
 }
--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { adminOnly, verifytoken } from '../utils/verifyUser.js';
-import { getUsers } from '../controller/user.controller.js';
+import { getUserById, getUsers } from '../controller/user.controller.js';
 
 const router = express.Router();
 
 // User management routes
 router.get("/getusers", verifytoken, adminOnly, getUsers);
+
+router.get("/:id", verifytoken,  getUserById);
 
 export default router;
