6cfbcdccbfa5817bbe366d60a290c65535361973
vishveshshukal109-collab <vishveshshukal109@gmail.com>
2026-04-15 00:33:10 +0530

    get task api route

--- Backend/controller/task.controller.js
+++ Backend/controller/task.controller.js
@@ -36,3 +36,78 @@ export const createTask =  async (req, res, next) => {
         next(error);
     }
 }
+
+export const getTasks = async (req, res, next) => {
+    try {
+        
+        const {status} = req.query;
+
+        let filter = {};
+
+        if(status){
+            filter.status = status;
+        }
+
+        let tasks;
+
+        if(req.user.role === 'admin'){
+            tasks = await Task.find(filter).populate(
+                'assignedTo', 
+                'name email profileImageUrl')
+        } else {
+            tasks = await Task.find({
+                 ...filter,
+                  assignedTo: req.user.id,
+                 }).populate('assignedTo', 'name email profileImageUrl');
+        }
+
+        tasks = await Promise.all(
+            tasks.map(async (task) => {
+                const completedCount = task.todoChecklist.filter(item => item.completed
+                ).length;
+
+                return {
+                    ...task._doc, completedCount: completedCount,}
+            })
+        );
+
+        // status summary count 
+
+        const allTasks = await Task.countDocuments(
+            req.user.role === 'admin' ? {} : { assignedTo: req.user.id }
+        );
+
+        const pendingTasks = await Task.countDocuments({
+            ...filter,
+            status: 'Pending',
+            // if logged in user is not admin then add assignedTo filter
+            // if logged in user is an admin then nothing to do, just count
+            ...(req.user.role !== 'admin' && { assignedTo: req.user.id }),
+        });
+
+        const inProgressTasks = await Task.countDocuments({
+            ...filter,
+            status: 'In-Progress',
+            ...(req.user.role !== 'admin' && { assignedTo: req.user.id }),
+        });
+
+        const completedTasks = await Task.countDocuments({
+            ...filter,
+            status: 'Completed',
+            ...(req.user.role !== 'admin' && { assignedTo: req.user.id }),
+        });
+        
+        res.status(200).json({ 
+            tasks,
+            statusSummary: { 
+                all : allTasks,
+                pendingTasks,
+                inProgressTasks,
+                completedTasks, 
+            },
+        });
+
+    } catch (error) {
+        next(error);
+    }
+}
--- Backend/routes/task.route.js
+++ Backend/routes/task.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { adminOnly, verifytoken } from '../utils/verifyUser.js';
-import { createTask } from '../controller/task.controller.js';
+import { createTask, getTasks } from '../controller/task.controller.js';
 
 const router = express.Router();
 
 router.post("/createTask", verifytoken, adminOnly, createTask);
+
+router.get("/", verifytoken, getTasks);
 
 export default router; 
