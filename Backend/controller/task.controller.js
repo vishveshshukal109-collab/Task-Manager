9d5455f88d9bbc4ff745bfe36ad15ab99f14c958
vishveshshukal109-collab <vishveshshukal109@gmail.com>
2026-04-16 01:02:31 +0530

    get user dashboard data api route

--- Backend/controller/task.controller.js
+++ Backend/controller/task.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Task from "../models/task.model.js";
 import { errorHandler } from "../utils/error.js";
 
@@ -340,5 +341,107 @@ export const getDashboardData = async (req, res, next) => {
         })
     } catch (error) {
         next(error);
+    }
+}
+
+export const userDashboardData = async (req, res, next) => {
+    try {
+        const userId = req.user.id
+
+        // console.log(userId)
+
+        // Convert userId to objectId for proper matching
+
+        const userObjectId = new mongoose.Types.ObjectId(userId)
+
+        // console.log(userObjectId);
+
+        // Fetch statistics for user - specific tasks
+
+        const totalTasks = await Task.countDocuments({ assignedTo: userId})
+
+        const pendingTasks = await Task.countDocuments({ 
+            assignedTo: userId,
+            status: "Pending",
+        })
+
+        const completedTasks = await Task.countDocuments({ 
+            assignedTo: userId,
+            status: "Completed",
+        })
+
+        const overdueTasks = await Task.countDocuments({ 
+            assignedTo: userId,
+            status: {$ne: "Completed"},
+            dueDate: {$lt: new Date() },
+        })
+
+        // Task distribution by status 
+
+        const taskStatuses = ["Pending","In-Progress","Completed"]
+
+        const taskDistributionRaw = await Task.aggregate([
+            {
+                $match: { assignedTo: userObjectId }
+            },
+            {
+                $group: { _id: "$status", count: { $sum: 1 } },
+            },
+        ])
+
+        console.log(taskDistributionRaw);
+
+        const taskDistribution = taskStatuses.reduce((acc, status) => {
+            const formattedkey = status.replace(/\s+/g, "")
+
+            acc[formattedkey] = taskDistributionRaw.find((item) => item._id === status )?.count || 0 
+
+            return acc 
+        },{})
+
+        taskDistribution["All"] = totalTasks
+
+        // Task Distribution by priority
+
+        const taskPriorities =["Low", "Medium", "High"]
+
+        const taskPriorityLevelRaw = await Task.aggregate ([
+            {
+                $match: { assignedTo: userObjectId } 
+            },
+            {
+                $group: {
+                    _id: "$priority",
+                    count: { $sum: 1 },
+                }
+            }
+        ])
+
+        const taskPriorityLevel = taskPriorities.reduce((acc, priority) => {
+            acc[priority] = taskPriorityLevelRaw.find((item) => item._id === priority)?. count || 0
+
+            return acc 
+        },{})
+
+        const recentTasks = await Task.find({ assignedTo: userObjectId })
+        .sort({createdAt: -1 })
+        .limit(10)
+        .select("title status priority dueDate createdAt")
+
+        res.status(200).json({
+            statistics: {
+                totalTasks,
+                pendingTasks,
+                completedTasks,
+                overdueTasks,
+            },
+            charts:{
+                taskDistribution,
+                taskPriorityLevel
+            },
+            recentTasks,
+        })
+    } catch (error) {  
+      next(error);  
     }
 }
--- Backend/routes/task.route.js
+++ Backend/routes/task.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { adminOnly, verifytoken } from '../utils/verifyUser.js';
-import { createTask, deleteTask, getDashboardData, getTaskById, getTasks, updateTask, updateTaskChecklist, updateTaskStatus } from '../controller/task.controller.js';
+import { createTask, deleteTask, getDashboardData, getTaskById, getTasks, updateTask, updateTaskChecklist, updateTaskStatus, userDashboardData } from '../controller/task.controller.js';
 
 const router = express.Router();
 
@@ -9,6 +9,8 @@ router.post("/createTask", verifytoken, adminOnly, createTask);
 router.get("/", verifytoken, getTasks);
 
 router.get("/dashboarddata", verifytoken, adminOnly, getDashboardData);
+
+router.get("/userdashboarddata",verifytoken, userDashboardData);
 
 
 
