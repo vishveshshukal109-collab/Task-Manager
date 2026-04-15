0b95843ff83debbcbe8e0cd1910f2affc20dba5f
vishveshshukal109-collab <vishveshshukal109@gmail.com>
2026-04-15 13:14:56 +0530

    update task status api route

--- Backend/controller/task.controller.js
+++ Backend/controller/task.controller.js
@@ -177,3 +177,35 @@ export const deleteTask = async (req, res, next) => {
         next(error);
     }
 }
+
+export const updateTaskStatus = async (req, res, next) => {
+    try {
+        const task = await Task.findById(req.params.id);
+
+        if (!task) {
+            return next(errorHandler(404, "Task not found"));
+        }
+         
+        const isAssigned = task.assignedTo.some(
+            (userId) => userId.toString() === req.user.id.toString()
+        );
+
+        if (!isAssigned && req.user.role !== 'admin') {
+            return next(errorHandler(403, "Unauthorized to update task status"));
+        }
+
+        task.status = req.body.status || task.status;
+
+        if(task.status === 'Completed'){
+            task.todoChecklist.forEach((item) => 
+                (item.completed = true));
+        }
+
+        await task.save();
+
+        res.status(200).json({message: "Task status updated successfully", task });
+
+    } catch (error) {
+        next(error);
+    }
+}
--- Backend/routes/task.route.js
+++ Backend/routes/task.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { adminOnly, verifytoken } from '../utils/verifyUser.js';
-import { createTask, deleteTask, getTaskById, getTasks, updateTask } from '../controller/task.controller.js';
+import { createTask, deleteTask, getTaskById, getTasks, updateTask, updateTaskStatus } from '../controller/task.controller.js';
 
 const router = express.Router();
 
@@ -13,5 +13,7 @@ router.get("/:id", verifytoken, getTaskById);
 router.put("/:id", verifytoken, updateTask);
 
 router.delete("/:id", verifytoken, adminOnly, deleteTask);
+
+router.put("/:id/status", verifytoken, updateTaskStatus);
 
 export default router; 
