e69aa29c2f464eb4a6d8c7dd7b90fe3ec133fef5
vishveshshukal109-collab <vishveshshukal109@gmail.com>
2026-04-17 18:13:44 +0530

    Login UI

---
+++ Frontend/src/components/AuthLayout.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const AuthLayout = ({ children }) => {
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <div className="w-full md:w-1/2 overflow-y-auto my-auto">
+        <div className="min-h-full flex flex-col px-12 pt-8 pb-12 ">
+          <div className="flex items-center justify-center">{children}</div>
+        </div>
+      </div>
+      <div className="hidden md:block w-1/2">
+        <img
+          src="https://images.pexels.com/photos/6991831/pexels-photo-6991831.jpeg?_gl=1*ppchl3*_ga*MTQzMDAwODc5OC4xNzc2MzQ2NTE2*_ga_8JE65Q40S6*czE3NzYzNDY1MTUkbzEkZzEkdDE3NzYzNDg4MjIkajU5JGwwJGgw"
+          alt="Login background"
+          className="h-full w-full object-cover"
+        />
+      </div>
+    </div>
+  );
+};
+export default AuthLayout;
--- Frontend/src/pages/auth/Login.jsx
+++ Frontend/src/pages/auth/Login.jsx
@@ -1,9 +1,116 @@
-import React from 'react'
+import React, { useState } from "react";
+import AuthLayout from "../../components/AuthLayout";
+import { BiLogoMicrosoftTeams } from "react-icons/bi";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa6";
+import {Link} from "react-router-dom";
+import { validateEmail } from "../../utils/helper";
+
+
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [showPassword, setShowPassword] = useState(false)
+  const [error, setError] = useState(null)
 
-export default Login
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if(!validateEmail(email)) {
+      setError("Please Enter a Vaild Email Address")
+      return
+    }
+
+    if(!password){
+      setError("Please Enter an Password")
+      return
+    }
+
+    setError(null)
+
+    // Login API call
+  }
+
+  return (
+    <AuthLayout>
+      <div className="w-full max-w-md">
+        <div className="bg-white rounded-xl shadow-2xl overflow-hidden">
+          {/* Gradient top border */}
+          <div className="h-2 bg-linear-to-r from-blue-600 to-blue-400"></div>
+
+          <div className="p-8 ">
+            {/*Logo and Title */}
+            <div className="text-center mb-8">
+              <div className="flex justify-center">
+                <div className="bg-blue-100 p-3 rounded-full">
+                  <BiLogoMicrosoftTeams className="text-4xl text-blue-700" />
+                </div>
+              </div>
+              <h1 className="text-2xl font-bold text-gray-700 mt-4 uppercase">
+                Taskify
+                </h1>
+                <p className="text-semibold text-gray-600 mt-1 uppercase" >Task & Project Management Tool</p>
+            </div>
+            {/* Login Form*/}
+            <form onSubmit={handleSubmit} className="space-y-6">
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">Email Address</label>
+
+                <input
+                  id="email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className = "w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                  placeholder="your@email.com"
+                  required
+                  />
+              </div>
+
+              <div>
+                <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">Password</label>
+
+               <div className="relative">
+                <input
+                  id="password"
+                  type={showPassword ? "text" : "password"}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className = "w-full px-4 py-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent pr-12"
+                  placeholder="••••••"
+                  required
+                  />
+
+                  <button type="button" className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-500 hover:text-gray-700"
+                  onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? <FaEyeSlash /> : <FaEye /> }
+                  </button>
+                </div>
+              </div>
+              {error && <p className="text-red-500 text-sm">{error}</p>}
+
+              <div>
+                <button type ="submit" className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-0 focus:ring-offset-0 cursor-pointer">
+                  LOGIN
+                </button>
+              </div>
+            </form>
+
+            <div className="mt-6 text-center text-sm">
+              <p className="text-gray-600">
+                Don't have an Account ?{" "}
+                <Link to={"/signup"} className="font-medium text-blue-600 hover:text-blue-500">
+                Sign up
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </AuthLayout>
+  );
+};
+
+export default Login;
