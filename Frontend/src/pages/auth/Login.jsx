a101213a7a8f247d46a54cc75dce720cbb2072c8
vishveshshukal109-collab <vishveshshukal109@gmail.com>
2026-04-28 19:57:38 +0530

    add react-redux

--- Frontend/src/pages/auth/Login.jsx
+++ Frontend/src/pages/auth/Login.jsx
@@ -6,17 +6,23 @@ import { FaEyeSlash } from "react-icons/fa6";
 import {Link, useNavigate} from "react-router-dom";
 import { validateEmail } from "../../utils/helper";
 import axiosInstance from "../../utils/axioInstance";
+import {useDispatch, useSelector} from "react-redux"
+import { signInStart, signInSuccess } from "../../redux/slice/userSlice";
 
 
 
 const Login = () => {
   const navigate = useNavigate()
+  const dispatch = useDispatch()
   
   
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [showPassword, setShowPassword] = useState(false)
   const [error, setError] = useState(null)
+
+
+  const {loading} = useSelector((state) => state.user)
 
   const handleSubmit = async(e) => {
     e.preventDefault()
@@ -35,6 +41,10 @@ const Login = () => {
 
     // Login API call
     try {
+      dispatch(signInStart())
+
+
+
       const response = await axiosInstance.post("/auth/signin",{
         email,
         password,
@@ -43,8 +53,10 @@ const Login = () => {
       //console.log(response.data)
 
       if(response.data.role === "admin"){
+        dispatch(signInSuccess(response.data))
         navigate("/admin/dashboard")
       }else{
+        dispatch(signInSuccess(response.data))
         navigate("/user/dashboard")
       }
 
@@ -117,11 +129,15 @@ const Login = () => {
               </div>
               {error && <p className="text-red-500 text-sm">{error}</p>}
 
-              <div>
+              {loading ? (
+                <span className="animate-pulse">Loading...
+                  </span>
+              ): (<div>
                 <button type ="submit" className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-0 focus:ring-offset-0 cursor-pointer uppercase">
                   LOGIN
                 </button>
-              </div>
+              </div> 
+            )}
             </form>
 
             <div className="mt-6 text-center text-sm">
