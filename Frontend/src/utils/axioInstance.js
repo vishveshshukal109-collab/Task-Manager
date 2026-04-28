4cdfe098c8ba2f6c163537a332723ec1efe6c2ba
vishveshshukal109-collab <vishveshshukal109@gmail.com>
2026-04-28 14:05:51 +0530

    Install axios and creating axios instance

---
+++ Frontend/src/utils/axioInstance.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+
+const   BASE_URL = "http://localhost:3000"
+
+const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    timeout: 10000,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+})
